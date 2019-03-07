537ba0e89e98c369f49a711562106c072e3e9bd7
Mike Lownds <cowplex@ananke>
2019-03-07 12:38:43 -0500

    Initial Commit
    Like, a lot.

---
+++ src/main/java/org/usfirst/frc1504/Robot2019/Elevator.java
@@ -0,0 +1,247 @@
+package org.usfirst.frc1504.Robot2019;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import org.usfirst.frc1504.Robot2019.Update_Semaphore.Updatable;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Preferences;
+
+public class Elevator implements Updatable {
+	// Elevator
+	public enum ELEVATOR_MODE {HATCH, CARGO, INIT};
+	private ELEVATOR_MODE _mode = ELEVATOR_MODE.INIT;
+
+	//private double[][] _bottom_setpoints = {{18.4, 10.4}, {18.4, 16.1}, {10.6, 12.5}, {10.6, 30.9}, {80.0, 74.3}};
+	//private double[][] _top_setpoints = {{12.0, 13.8}, {12.0, 58.8}, {24.5, 40.3}, {72.4, 63.9}, {71.0, 72.5}};
+	private double[][] _bottom_setpoints = {{18.4, 10.4}, {18.4, 16.1}, {10.6, 12.5}, {10.6, 30.9}, {80.0, 74.3}};
+	private double[][] _top_setpoints = {{13.0, 13.8}, {13.0, 58.8}, {23.8, 41.3}, {72.4, 64.9}, {71.0, 73.5}};
+	private boolean _elevator_enable = false;
+	private int _setpoint = 0;
+
+	private int _last_input = -1;
+	private boolean _last_mode_input = false;
+	private int _override_setpoint_count = 0;
+
+	private WPI_TalonSRX _bottom_actuator;
+	private WPI_TalonSRX _top_actuator;
+
+	public boolean lastElevatorButtonState = false;
+
+	private static final Elevator instance = new Elevator();
+	private DriverStation _ds = DriverStation.getInstance();
+
+	private Potentiometer _bottom_potentiometer;
+	private Potentiometer _top_potentiometer;
+
+	public static Elevator getInstance() // sets instance
+	{
+		return instance;
+	}
+
+	private Elevator() // Elevator constructor
+	{
+		AnalogInput a = new AnalogInput(Map.BOTTOM_POTENTIOMETER_PORT);
+		_bottom_potentiometer = new AnalogPotentiometer(a, 100, 0);
+
+		AnalogInput b = new AnalogInput(Map.TOP_POTENTIOMETER_PORT);
+		_top_potentiometer = new AnalogPotentiometer(b, 100, 0);
+
+		_bottom_actuator = new WPI_TalonSRX(Map.BOTTOM_ACTUATOR_PORT);
+		_top_actuator = new WPI_TalonSRX(Map.TOP_ACTUATOR_PORT);
+
+		Preferences p = Preferences.getInstance();
+		int i, j;
+		for(i = 0; i < _top_setpoints.length; i++)
+		{
+			for(j = 0; j < _top_setpoints[i].length; j++)
+			{
+				if(!p.containsKey("Elevator_top_" + i + "_" + j))
+					p.putDouble("Elevator_top_" + i + "_" + j, _top_setpoints[i][j]);
+				if(!p.containsKey("Elevator_bottom_" + i + "_" + j))
+					p.putDouble("Elevator_bottom_" + i + "_" + j, _bottom_setpoints[i][j]);
+				
+				_top_setpoints[i][j] = p.getDouble("Elevator_top_" + i + "_" + j, _top_setpoints[i][j]);
+				_bottom_setpoints[i][j] = p.getDouble("Elevator_bottom_" + i + "_" + j, _bottom_setpoints[i][j]);
+			}
+		}
+
+		Update_Semaphore.getInstance().register(this);
+		System.out.println("Elevator initialized");
+	}
+
+	public static void initialize() // initialize
+	{
+		getInstance();
+	}
+
+	public ELEVATOR_MODE getMode()
+	{
+		return _mode;
+	}
+
+	private void compute_nearest_setpoint()
+	{
+		int i;
+		for(i = 0; i < _top_setpoints.length; i++)
+		{
+			if(Math.abs(_top_setpoints[i][_mode.ordinal()] - _top_potentiometer.get()) < 3.0 &&
+				Math.abs(_bottom_setpoints[i][_mode.ordinal()] - _bottom_potentiometer.get()) < 3.0)
+			{
+				_setpoint = i;
+				_elevator_enable = true;
+				return;
+			}
+		}
+		_setpoint = 0;
+		_elevator_enable = false;
+	}
+
+	private void compute_mode()
+	{
+		/*if(IO.toggle_elevator_mode() && !_last_mode_input)
+		{
+			if(_mode == ELEVATOR_MODE.CARGO)
+				_mode = ELEVATOR_MODE.HATCH;
+			else if(_mode == ELEVATOR_MODE.HATCH && Hatch.getInstance().getState() != Hatch.HATCH_STATE.HOLDING)
+				_mode = ELEVATOR_MODE.CARGO;
+		}
+		_last_mode_input = IO.toggle_elevator_mode();*/
+		if(IO.elevator_mode() == ELEVATOR_MODE.HATCH)
+			_mode = ELEVATOR_MODE.HATCH;
+		else if(IO.elevator_mode() == ELEVATOR_MODE.CARGO && (Hatch.getInstance().getState() != Hatch.HATCH_STATE.HOLDING || IO.override()))
+			_mode = ELEVATOR_MODE.CARGO;
+	}
+
+	private void compute_setpoint()
+	{
+		if(IO.override())
+			return;
+		
+		int input = IO.hid();
+		
+		if(input != -1)
+			_elevator_enable = true;
+
+		switch(input)
+		{
+			case -1: // No input
+				break;
+			case 270: // HOME 1
+				_setpoint = 0;
+				break;
+			case 90: // HOME 2
+				_setpoint = 1;
+				break;
+			default:
+				if(_last_input != -1)
+					break;
+				if(input == 0) // up
+				{
+					if(_setpoint < 2)
+					{
+						_setpoint = 2;
+						break;
+					}
+					if(_setpoint < _top_setpoints.length - 1)
+						_setpoint++;
+				}
+				else if(input == 180)
+				{
+					if(_setpoint > 0)
+						_setpoint--;
+				}
+		}
+		_last_input = input;
+	}
+
+	private void update()
+	{
+		if(!_elevator_enable || _mode == ELEVATOR_MODE.INIT)
+			return;
+		
+		double top_error = (_top_setpoints[_setpoint][_mode.ordinal()] - _top_potentiometer.get());
+		double bottom_error = (_bottom_setpoints[_setpoint][_mode.ordinal()] - _bottom_potentiometer.get());
+		
+		if(top_error < 0.0 && _bottom_potentiometer.get() < Map.SWING_BOTTOM_SAFEZONE && Math.abs(bottom_error) > Map.SWING_SAFEZONE_TOLERANCE)
+			_top_actuator.set(0.0);
+		else
+			_top_actuator.set(top_error * Map.ELEVATOR_GAIN);
+
+		// Don't run bottom actuator up unless the top arm won't intersect the post
+		if(bottom_error > 0.0 && _top_potentiometer.get() < Map.SWING_TOP_SAFEZONE && _top_actuator.get() != 0.0)
+			_bottom_actuator.set(0.0);
+		else
+			_bottom_actuator.set(bottom_error * Map.ELEVATOR_GAIN);
+	}
+
+	private void update_dashboard()
+	{
+		//uddb
+		SmartDashboard.putString("Elevator Mode", _mode.toString());
+		SmartDashboard.putBoolean("Elevator Enabled", _elevator_enable);
+		SmartDashboard.putNumber("Elevator Setpoint", _setpoint);
+		SmartDashboard.putNumber("Elevator Top Actuator Position", _top_potentiometer.get());
+		SmartDashboard.putNumber("Elevator Bottom Actuator Position", _bottom_potentiometer.get());
+		if(_mode.ordinal() < _top_setpoints[0].length)
+		{
+			SmartDashboard.putNumber("Elevator Top Actuator Commanded Position", _top_setpoints[_setpoint][_mode.ordinal()]);
+			SmartDashboard.putNumber("Elevator Bottom Actuator Commanded Position", _bottom_setpoints[_setpoint][_mode.ordinal()]);
+			SmartDashboard.putNumber("Elevator Top Actuator Position Error", (_top_setpoints[_setpoint][_mode.ordinal()] - _top_potentiometer.get()));
+			SmartDashboard.putNumber("Elevator Bottom Actuator Position Error", (_bottom_setpoints[_setpoint][_mode.ordinal()] - _bottom_potentiometer.get()));
+		}
+	}
+
+	public void semaphore_update() // updates robot information
+	{
+		update_dashboard();
+
+		if (_ds.isDisabled()) // only runs in teleop
+		{
+			_mode = ELEVATOR_MODE.INIT;
+			_elevator_enable = false;
+			return;
+		}
+
+		if(_mode == ELEVATOR_MODE.INIT)
+		{
+			if(Hatch.getInstance().getState() == Hatch.HATCH_STATE.DISABLED)
+				return;
+			_mode = Hatch.getInstance().getState() == Hatch.HATCH_STATE.CLOSED ? ELEVATOR_MODE.CARGO : ELEVATOR_MODE.HATCH;
+			compute_nearest_setpoint();
+		}
+		
+		if(IO.override())
+		{
+			_elevator_enable = false;
+			_bottom_actuator.set(IO.get_bottom_actuator_speed() * .7);
+			_top_actuator.set(IO.get_top_actuator_speed() * .7);
+
+			if(_mode != ELEVATOR_MODE.INIT)
+			{
+				if(IO.hid() != -1)
+					_override_setpoint_count++;
+				else
+					_override_setpoint_count = 0;
+				
+				if(_override_setpoint_count == 40) // > 800 milliseconds
+				{
+					_top_setpoints[_setpoint][_mode.ordinal()] = _top_potentiometer.get();
+					_bottom_setpoints[_setpoint][_mode.ordinal()] = _bottom_potentiometer.get();
+					Preferences.getInstance().putDouble("Elevator_top_" + _setpoint + "_" + _mode.ordinal(), _top_setpoints[_setpoint][_mode.ordinal()]);
+					Preferences.getInstance().putDouble("Elevator_bottom_" + _setpoint + "_" + _mode.ordinal(), _bottom_setpoints[_setpoint][_mode.ordinal()]);
+					
+					System.out.println("Elevator setpoint for position " + _setpoint + " updated");
+				}
+			}
+		}
+
+		compute_mode();
+		compute_setpoint();
+		update();
+	}
+}
---
+++ src/main/java/org/usfirst/frc1504/Robot2019/Lift.java
@@ -0,0 +1,144 @@
+package org.usfirst.frc1504.Robot2019;
+
+import org.usfirst.frc1504.Robot2019.Update_Semaphore.Updatable;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Servo;
+
+public class Lift implements Updatable
+{
+	public enum LIFT_STATE {RETRACT, EXTEND, FRONT_UP};
+	private LIFT_STATE _state = LIFT_STATE.RETRACT;
+
+    private static final Lift instance = new Lift();
+    private DriverStation _ds = DriverStation.getInstance();
+
+   	// We're in the endgame now
+	private DoubleSolenoid _lift_front;
+	private DoubleSolenoid _lift_back;
+
+	private int _last_input = 0;
+	private boolean _lockout;
+	
+
+	private BuiltInAccelerometer _accelerometer = new BuiltInAccelerometer(Accelerometer.Range.k8G);
+	private Servo _front_servo;
+	private Servo _back_servo;
+	
+    public static Lift getInstance() // sets instance
+	{
+		return instance;
+    }
+
+    private Lift() // Lift constructor
+    {
+        _lift_front = new DoubleSolenoid(Map.END_LIFT_FRONT_HIGHSIDE_PORT, Map.END_LIFT_FRONT_LOWSIDE_PORT);
+		_lift_back = new DoubleSolenoid(Map.END_LIFT_BACK_HIGHSIDE_PORT, Map.END_LIFT_BACK_LOWSIDE_PORT);
+		
+		_front_servo = new Servo(Map.LEFT_SERVO_PORT);
+		_back_servo = new Servo(Map.RIGHT_SERVO_PORT);
+
+		_lockout = false;
+        
+		Update_Semaphore.getInstance().register(this);
+		System.out.println("Lift initialized");
+    }
+    
+    public static void initialize() // initialize
+    {
+        getInstance();
+    }
+	
+	public LIFT_STATE getState()
+	{
+		return _state;
+	}
+
+	public double getAngle()
+	{
+		return Math.atan2(-_accelerometer.getX(), _accelerometer.getY());
+	}
+
+	private void servo_adjustment()
+	{
+		if(_state == LIFT_STATE.EXTEND)
+		{
+			_front_servo.set(getAngle() * Map.SERVO_GAIN_VALUE);
+			_back_servo.set(getAngle() * -Map.SERVO_GAIN_VALUE);
+		}
+		else
+		{
+			_front_servo.set(0.0);
+			_back_servo.set(0.0);
+		}
+	}	
+
+	private void update_state()
+	{
+		DoubleSolenoid.Value front = DoubleSolenoid.Value.kReverse;
+		DoubleSolenoid.Value rear = DoubleSolenoid.Value.kReverse;
+
+		if(_state == LIFT_STATE.EXTEND)
+			front = DoubleSolenoid.Value.kForward;
+		
+		if(_state == LIFT_STATE.EXTEND || _state == LIFT_STATE.FRONT_UP)
+			rear = DoubleSolenoid.Value.kForward;
+		
+		// Tip correction - Failsafe
+		if(_state == LIFT_STATE.EXTEND && Math.abs(getAngle()) > 10.0) // TIPPING A LOT
+		{
+			if(Math.signum(getAngle()) > 0)
+				front = DoubleSolenoid.Value.kReverse;
+			else
+				rear = DoubleSolenoid.Value.kReverse;
+		}
+		
+		_lift_front.set(front);
+		_lift_back.set(rear);
+	}
+
+	private void update_dashboard()
+	{
+		//uddb
+		SmartDashboard.putString("Lift State", _state.toString());
+		SmartDashboard.putBoolean("Lift Lockout", _lockout);
+		SmartDashboard.putNumber("Lift Front Air Restrictor", _front_servo.get());
+		SmartDashboard.putNumber("Lift Back Air Restrictor", _back_servo.get());
+		SmartDashboard.putNumber("Lift Accelerometer Y", _accelerometer.getY());
+		SmartDashboard.putNumber("Lift Accelerometer X", _accelerometer.getX());
+		SmartDashboard.putNumber("Lift Accelerometer Abgle", getAngle());
+	}
+
+    public void semaphore_update() // updates robot information
+	{
+		update_dashboard();
+		
+		if (_ds.isDisabled()) // only runs in teleop
+		{
+			_state = LIFT_STATE.RETRACT;
+			_lockout = false;
+			return;
+		}
+
+		// Must have pressed one button then both together to make endgame lift fire
+		// Must release both buttons to move on to the next position in sequence
+		if(!_lockout && IO.get_lift_activation() == 3 && (_last_input == 1 || _last_input == 2))
+		{
+			_state = (_state.ordinal() + 1 >= LIFT_STATE.values().length) ? 
+				LIFT_STATE.RETRACT :
+				LIFT_STATE.values()[_state.ordinal() + 1];
+			_lockout = true;
+		}
+		else if (IO.get_lift_activation() == 0)
+		{
+			_lockout = false;
+		}
+		_last_input = IO.get_lift_activation();	
+
+		update_state();
+		servo_adjustment();
+	}
+}
